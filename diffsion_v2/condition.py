81f99555508d3198005ae9ac2df6168edfcd6353
AnsonNing <139607242+AnsonNing@users.noreply.github.com>
2025-05-29 15:42:23 +0900

    Delete diffsion_v2/condition.py

--- diffsion_v2/condition.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class LabelEmbedding(nn.Module):
-    def __init__(self, num_classes, embedding_dim):
-        super().__init__()
-        self.label_emb = nn.Embedding(num_classes, embedding_dim)
-
-    def forward(self, y):
-        return self.label_emb(y)
